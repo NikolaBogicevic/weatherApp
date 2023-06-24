8bdfc9f415e8c73defa8d4a21eeb721f59065fdb
NikolaBogicevic <nikola.bogicevic1984@gmail.com>
2023-06-25 00:05:20 +0200

    Add logic asd styles

--- app.js
+++ app.js
@@ -1,20 +1,69 @@
-let input = document.getElementById("input").value;
+
 
 const apiKey = "88daa433ef2720828eadc893be740ea4";
-
+const temp = document.querySelector(".temperature");
+const windSpeed = document.querySelector(".wind-speed");
+const humidity = document.querySelector(".humidity");
+const description = document.querySelector(".description");
+const mainContent = document.querySelector(".main-content");
+const notFound = document.querySelector(".not-found");
+const card = document.querySelector(".card");
 
 document.getElementById("button").addEventListener("click", async ()=>{
-    console.dir(document.getElementById("input"))
-    try{ const api = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${apiKey}`;
+    let input = document.querySelector("input").value; 
+    const weatherPicture = document.querySelector(".weather-picture");
+
+    try{  
+        const api = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${apiKey}`;// getting data
  
-     const response = await fetch(api);
+        const response = await fetch(api);
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
  
-     if(response.status === 200){
-         const cities = await response.json();
-         const city = cities.filter((city) => city.name === "Serbia")
-        //  console.log(input)
-     }}catch(err){
-         console.log(err)
+        if(response.status === 200){
+            mainContent.style.display = "flex";
+            card.style.height = "550px";
+        notFound.style.display = "none";
+         const city = await response.json(); 
+
+         const temperature = city.main.temp;
+         temp.innerHTML = temperature + "&#8451";
+
+         const speed = city.wind.speed;
+         windSpeed.innerHTML = speed + "Km/h <br>Wind Speed";
+
+         const humidityValue = city.main.humidity;
+        humidity.innerHTML = humidityValue + "% <br>Humidity";
+
+        const descriptionValue = city.weather[0].description;
+        description.innerHTML = descriptionValue;
+
+        const weatherPictureValue = city.weather[0].main;
+
+        if (weatherPictureValue === "Clear") {
+            weatherPicture.src = "images/clear.png";
+        } 
+        else if (weatherPictureValue === "Clouds") {
+            weatherPicture.src = "images/cloud.png";
+        } 
+        else if (weatherPictureValue === "Mist") {
+            weatherPicture.src = "images/mist.png";
+        } 
+        else if (weatherPictureValue === "Rain") {
+            weatherPicture.src = "images/rain.png";
+        } 
+        else if (weatherPictureValue === "Snow") {
+            weatherPicture.src = "images/snow.png";
+        } 
+
+     }}catch(err){  
+        if(input === "") return alert("Please enter a valid city!"); 
+
+        card.style.height = "550px";
+        mainContent.style.display = "none";
+        notFound.style.display = "flex";
      }
  
  } )
